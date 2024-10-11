ce9c2d7985e47909fb807deb70270e7a6b4343ed
vbg03 <valeria.bowers@uao.edu.co>
2024-10-10 21:05:46 -0500

    sus

--- src/helpers/Operaciones.js
+++ src/helpers/Operaciones.js
@@ -1,15 +1,18 @@
-export const Operaciones = (numeros, setNumeros)=>{
-    const {numero1, numero2} = numeros;
-    const handleChange=(e)=>{
+export const Operaciones = (numeros, setNumeros) => {
+    const { numero1, numero2 } = numeros;
+
+    const handleChange = (e) => {
+        const value = e.target.value === '' ? 0 : parseFloat(e.target.value);
         setNumeros({
             ...numeros,
-            [e.target.name]:parseFloat([e.target.value])
+            [e.target.name]: value
         });
-        
-    }
-    const suma =()=> numero1 + numero2;
-    const resta =()=> numero1 - numero2;
-    const division =()=> numero1 / numero2;
-    const multiplicacion =()=> numero1 * numero2;
-    return{handleChange, suma, resta, division, multiplicacion, numero1, numero2}
-}
+    };
+
+    const suma = () => numero1 + numero2;
+    const resta = () => numero1 - numero2;
+    const division = () => (numero2 !== 0 ? numero1 / numero2 : 'No se puede dividir entre 0');
+    const multiplicacion = () => numero1 * numero2;
+
+    return { handleChange, suma, resta, division, multiplicacion, numero1, numero2 };
+};
